83fda3fdb18e37515526733e069949d5066010d9
wayne-yang7021 <wayne.yang7021@gmail.com>
2025-07-29 01:09:28 +0800

    change file structure and finish one version of detectron

---
+++ src/modules/detection/detection.py
@@ -0,0 +1,26 @@
+import numpy as np
+from PIL import Image
+from typing import List, Dict
+
+class ObjectDetector:
+    def __init__(self, model):
+        """初始化：傳入已載入的模型物件"""
+        self.predictor = model.detector
+        self.class_names = model.coco_classes
+
+    def detect_objects_in_image(self, image: Image.Image) -> List[Dict]:
+        """使用 Detectron2 對圖像執行物體偵測"""
+        img_array = np.array(image.convert("RGB"))
+        outputs = self.predictor(img_array)
+
+        instances = outputs["instances"].to("cpu")
+        results = []
+        for i in range(len(instances)):
+            result = {
+                "label": self.class_names[instances.pred_classes[i]],
+                "score": float(instances.scores[i]),
+                "box": instances.pred_boxes[i].tensor.numpy()[0].tolist(),
+                "mask": instances.pred_masks[i].numpy()
+            }
+            results.append(result)
+        return results
---
+++ src/modules/models/models.py
@@ -0,0 +1,20 @@
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2 import model_zoo
+from detectron2.data import MetadataCatalog
+
+class Model:
+    def init(self):
+        self.detectron = None
+
+    def load_detectron(self):
+        cfg = get_cfg()
+        cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml"))
+        cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml")
+        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.3
+        cfg.MODEL.DEVICE = "cpu"
+        self.detector = DefaultPredictor(cfg)
+        # Get dataset name from config, not from DefaultPredictor
+        dataset_name = cfg.DATASETS.TRAIN[0]
+        self.coco_classes = MetadataCatalog.get(dataset_name).thing_classes
+        return self.detector, self.coco_classes
